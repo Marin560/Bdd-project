141c5fd2c023f3cd15325d9022bbfe089dae8795
Unknown <marin.boone@edu.ece.fr>
2018-04-21 19:05:01 +0200

    Possibilité de changer de surveillant (chambre)

--- src/Vue/EcranPrincipal.java
+++ src/Vue/EcranPrincipal.java
@@ -12,6 +12,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
 import java.awt.event.ItemEvent; 
 import java.awt.event.ItemListener; 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,17 +24,22 @@ import java.util.logging.Logger;
 import javax.swing.JButton; 
 import javax.swing.JComboBox;
 import javax.swing.JFrame; 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel; 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.event.*;
+import javax.swing.table.TableModel;
 
 /**
  *
  * @author Roxane
  */
   
-public class EcranPrincipal extends Fenetre implements ActionListener, ItemListener{ 
+public class EcranPrincipal extends Fenetre implements ActionListener, ItemListener,TableModelListener,MouseListener{ 
      
     private JButton Recherche, MaJ, Reporting, modif; //Bouttons de la fenêtre
     //private MonBouton Recherche, MaJ, Reporting;
@@ -46,13 +54,14 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
     public String namebdd;
     private Connection conn; ///////////
     
-    public EcranPrincipal(Connection maConnexion) throws SQLException{ //Constructeur
+    public EcranPrincipal (Connection maConnexion) throws SQLException{ //Constructeur
         // creation par heritage de la fenetre 
         super("Logiciel de Gestion du Centre Hospitalier",800,750); 
         setResizable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Quitte le programme quand la fenêtre est fermée
 
         this.conn = maConnexion;
+        Statement stmt = maConnexion.createStatement(); 
         
         
         //On crée le panneau déroulant
@@ -66,6 +75,57 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
         combo.addItem("Soigne");
         combo.addActionListener(this);
          
+        ////////////////////////////
+        
+        jtable.addMouseListener(new MouseAdapter()  {
+			public void mousePressed(MouseEvent e) {
+ 
+                                // clic sur le bouton gauche ou droit
+				if(e.getButton() == MouseEvent.BUTTON1 ||
+						e.getButton() == MouseEvent.BUTTON3) 
+				{
+					int row = jtable.rowAtPoint(e.getPoint()); 
+					int col = jtable.columnAtPoint(e.getPoint());
+					
+                                        //On vérifie la colonne sélectionnée 
+                                        //jtable.getColumnName(col)
+                                        String ancienne_valeur = (String) jtable.getValueAt(row, col);
+                                        
+                                        System.out.println(jtable.getColumnName(col));
+                                        //System.out.println(combo.getSelectedItem());
+                                        
+                                        JOptionPane op = new JOptionPane();
+                                        String nom ;  
+                                        
+                                        //Pour la chambre
+                                        //Si on clique sur la bonne colonne, ici c'est surveillant 
+                                        if(jtable.getColumnName(col).equals("surveillant")){
+                                            //On demande la nouvelle valeur
+                                            nom = op.showInputDialog(null, "Entrez le nouveau Surveillant", "Changement de Surveillant", JOptionPane.QUESTION_MESSAGE);
+
+                                            try {
+                                                //UPDATE `chambre` SET `surveillant` = '86' WHERE CONCAT(`chambre`.`no_chambre`) = '101'
+                                                //stmt.execute("UPDATE '"+maString+"' SET '"+jtable.getColumnName(col)+"' = '"+nom+"' WHERE CONCAT(`chambre`.`"+jtable.getColumnName(col-1)+"`) = '"+jtable.getValueAt(row, col-1)+"' ");
+                                                stmt.execute("UPDATE `"+combo.getSelectedItem()+"` SET `"+jtable.getColumnName(col)+"` = '"+nom+"' WHERE CONCAT(`chambre`.`"+jtable.getColumnName(col-1)+"`) = '101'");
+                                                //On met la valeur dans le tableau
+                                                jtable.setValueAt(nom, row, col);
+                                            
+                                            } catch (SQLException ex) {
+                                                Logger.getLogger(EcranPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+                                            }
+
+                                            
+                                        }
+                                        
+                                        //Modification dans la base de donnée
+                                        //UPDATE `malade` SET `tel` = '0516545' WHERE CONCAT(`malade`.`numero`) = '0';
+                                        
+                                        
+                                }
+                                
+                        }
+        });
+        
         
         JScrollPane jScrollPane = new JScrollPane();
         jScrollPane.setViewportView(jtable);
@@ -83,7 +143,7 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
         Reporting.addActionListener(this);
         
         DefaultTableModel table = new DefaultTableModel();
-        Statement stmt = maConnexion.createStatement(); 
+        
         
         
         //Recherche des noms des colonnes pour la table "chambre"
@@ -106,7 +166,9 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
         }
         
         jtable.setModel(table);
-     
+        
+       
+        
         // Création des pannels  
         p0 = new JPanel(); 
         p1 = new JPanel(); 
@@ -154,22 +216,22 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
            
                  while(Requete.next())
                  {
-                     //System.out.println(Requete.getString("column_name"));
+                     //On ajoute au tableau chaque colonne de la table sélectionne
                      table.addColumn(Requete.getString("column_name"));
                  }
              
+                //On crée notre jtable en fonction du tableau crée
                 jtable.setModel(table);
              
       
-                 Requete = stmt.executeQuery("SELECT * FROM "+combo.getSelectedItem()+"");
+                Requete = stmt.executeQuery("SELECT * FROM "+combo.getSelectedItem()+"");
                 
-                 if(jtable.getColumnCount()== 2){
-                 while(Requete.next())
-                 {
+                if(jtable.getColumnCount()== 2){
+                while(Requete.next()){
                      table.addRow(new Object[]{
                          Requete.getString(table.getColumnName(0)),Requete.getString(table.getColumnName(1))
                      });
-                 }
+                    }
                 }
                  
                  if(jtable.getColumnCount()== 4){
@@ -181,9 +243,9 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
                  }
                 }
                  
-                 if(jtable.getColumnCount()== 5){
-                 while(Requete.next())
-                 {
+                if(jtable.getColumnCount()== 5){
+                while(Requete.next())
+                {
                      table.addRow(new Object[]{
                          Requete.getString(table.getColumnName(0)),Requete.getString(table.getColumnName(1)),Requete.getString(table.getColumnName(2)),Requete.getString(table.getColumnName(3)),Requete.getString(table.getColumnName(4))
                      });
@@ -198,10 +260,9 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
                      });
                  }
                 }
-                
-                
-             
+
              jtable.setModel(table);
+             
          } catch (SQLException ex) {
              Logger.getLogger(EcranPrincipal.class.getName()).log(Level.SEVERE, null, ex);
          }
@@ -226,6 +287,55 @@ public class EcranPrincipal extends Fenetre implements ActionListener, ItemListe
     public void itemStateChanged(ItemEvent ie) { 
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates. 
     } 
+
+    @Override
+    public void tableChanged(TableModelEvent tme) {
+        //On regarde les modifications de la table
+        System.out.println("Oui");
+        int row = tme.getFirstRow();
+        int column = tme.getColumn();
+        System.out.println(jtable.getValueAt(row, column));
+        
+        
+        TableModel model = (TableModel)tme.getSource();
+        
+        String columName = model.getColumnName(column);
+        Object data = model.getValueAt(row, column);
+        
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        if(me.getSource()== jtable){
+            int row = jtable.getSelectedRow();
+            int column = jtable.getSelectedColumn();
+            
+            System.out.println("Oui");
+            System.out.println(row);
+            System.out.println(column);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 } 
 
 
--- src/Vue/Essai.java
+++ src/Vue/Essai.java
@@ -17,6 +17,7 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
@@ -25,106 +26,16 @@ import javax.swing.SpringLayout;
  * @author peti_
  */
 public class Essai extends Fenetre implements ActionListener{
-
-    //Panel
-    JPanel panel_content = new JPanel();
-    JPanel panel_1 = new JPanel();
-    JPanel panel_2 = new JPanel();
-    //Bouton
-    JButton bouton1 = new JButton("Vers panel 2");
-    JButton bouton2 = new JButton("Vers panel 1");
-    //CardLayout
-    CardLayout cd = new CardLayout();
     
+    private JTable table;
     public Essai(){
-        JPanel panel = new JPanel();
-        GroupLayout layout = new GroupLayout(panel);
-        panel.setLayout(layout);
-        
-        JLabel label1 = new JLabel("Prenom");
-        JLabel label2 = new JLabel("Nom");
-        JLabel label3 = new JLabel("Telephone");
-        JLabel label4 = new JLabel("Mutuelle");
-        
-        JTextField tf1 = new JTextField();
-        JTextField tf2 = new JTextField();
-        JTextField tf3 = new JTextField();
-        JTextField tf4 = new JTextField();
-        
-        // Turn on automatically adding gaps between components
-        layout.setAutoCreateGaps(true);
 
-        // Turn on automatically creating gaps between components that touch
-        // the edge of the container and the container.
-        layout.setAutoCreateContainerGaps(true);
-
-        // Create a sequential group for the horizontal axis.
-
-        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
-
-        // The sequential group in turn contains two parallel groups.
-        // One parallel group contains the labels, the other the text fields.
-        // Putting the labels in a parallel group along the horizontal axis
-        // positions them at the same x location.
-        //
-        // Variable indentation is used to reinforce the level of grouping.
-        hGroup.addGroup(layout.createParallelGroup().
-            addComponent(label1).addComponent(label2).addComponent(label3).addComponent(label4));
-        hGroup.addGroup(layout.createParallelGroup().
-                 addComponent(tf1).addComponent(tf2).addComponent(tf3).addComponent(tf4));
-        layout.setHorizontalGroup(hGroup);
-
-        // Create a sequential group for the vertical axis.
-        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
-
-        // The sequential group contains two parallel groups that align
-        // the contents along the baseline. The first parallel group contains
-        // the first label and text field, and the second parallel group contains
-        // the second label and text field. By using a sequential group
-        // the labels and text fields are positioned vertically after one another.
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
-                 addComponent(label1).addComponent(tf1));
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
-                 addComponent(label2).addComponent(tf2));
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
-                 addComponent(label3).addComponent(tf3));
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
-                 addComponent(label4).addComponent(tf4));
-        layout.setVerticalGroup(vGroup);
+        super("Essai");
         
-        add(panel);
+        table = new JTable();
         
-        /*
-         panel_content.setLayout(cd);
-         
-         panel_1.add(bouton1);
-         panel_2.add(bouton2);
-         panel_1.setBackground(Color.BLUE);
-         panel_2.setBackground(Color.RED);
-         
-         panel_content.add(panel_1, "1");
-         panel_content.add(panel_2, "2");
-         cd.show(panel_content,"1");
-         
-         bouton1.addActionListener(new ActionListener() {
-             @Override
-             public void actionPerformed(ActionEvent ae) {
-                cd.show(panel_content, "2");
-             }
-         });
-        
-        bouton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                cd.show(panel_content, "1");
-            }
-        });
-             
-        add(panel_content);
-        pack();
-        
-        */
-        }
+    
+    }
 
     @Override
     
